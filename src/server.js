12e429692bfe23cb94e61fec2dbb5b1577b379ca
Lim Geun <shing100@naver.com>
2021-08-10 23:39:50 +0900

    recap

--- src/server.js
+++ src/server.js
@@ -16,12 +16,18 @@ const handleListen = () => console.log(`Listening on http://localhost:3000`);
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
+const onSocketClose = () => {
+  console.log("Disconnected from the Browser ❌");
+}
+
+const onSocketMessage = (message) => {
+  console.log(message.toString('utf8'));
+}
+
 wss.on("connection", (socket) => {
   console.log("Connected to Browser ✅");
-  socket.on("close", () => console.log("Disconnected from the Browser ❌"));
-  socket.on("message", (message) => {
-    console.log(message.toString('utf8'));
-  });
+  socket.on("close", onSocketClose);
+  socket.on("message", onSocketMessage);
   socket.send("hello!!!");
 });
 
