ddac403127e20fe059999c5bf96df8142a4360d1
Lim Geun <shing100@naver.com>
2021-08-18 16:36:49 +0900

    socket io

--- src/server.js
+++ src/server.js
@@ -1,33 +1,27 @@
-import http from "http"
-import WebSocket from "ws";
-import express from "express"
+import http from "http";
+import SocketIO from "socket.io";
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.set("view engine", "pug")
+app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 app.use("/public", express.static(__dirname + "/public"));
-
 app.get("/", (_, res) => res.render("home"));
 app.get("/*", (_, res) => res.redirect("/"));
 
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
-const server = http.createServer(app);
+wsServer.on("connection", (socket) => {
+  console.log(socket);
+});
+
+/*
 const wss = new WebSocket.Server({ server });
-
-const onSocketClose = () => {
-  console.log("Disconnected from the Browser ❌");
-}
-
 const sockets = [];
-
-const onSocketMessage = (message) => {
-  console.log(message.toString('utf8'));
-}
-
 wss.on("connection", (socket) => {
-  sockets.push(socket)
+  sockets.push(socket);
   socket["nickname"] = "Anon";
   console.log("Connected to Browser ✅");
   socket.on("close", onSocketClose);
@@ -38,12 +32,11 @@ wss.on("connection", (socket) => {
         sockets.forEach((aSocket) =>
           aSocket.send(`${socket.nickname}: ${message.payload}`)
         );
-        break;
       case "nickname":
         socket["nickname"] = message.payload;
-        break;
     }
   });
-});
+}); */
 
-server.listen(3000, handleListen);
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+httpServer.listen(3000, handleListen);
